b6e47efc4f7ffd8cd595add57a2780c83c849794
Wan Yin Lim <54065563+yinyin924@users.noreply.github.com>
2021-08-13 21:30:48 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        showLine();
+        System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?");
+        showLine();
+        showFarewell();
+    }
+
+    public static void showLine() {
+        System.out.println("_____________________________________________________________");
+    }
+
+    public static void showFarewell() {
+        System.out.println("Bye. Hope to see you again soon!");
+        showLine();
     }
 }
