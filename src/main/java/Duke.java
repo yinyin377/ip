4a69c830434ee1249dd48c94d7a5d867f6ad8da5
yinyin377 <54065563+yinyin924@users.noreply.github.com>
2021-08-13 22:40:39 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,17 +10,39 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         showLine();
-        System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?");
+        System.out.println("\tHello! I'm Duke" + "\n\t" + "What can I do for you?");
         showLine();
+        boolean toExit = false;
+        Scanner scan = new Scanner(System.in);
+        while (!toExit) {
+            try {
+                String command = readCommand(scan);
+                if (!command.equals("bye")){
+                    showLine();
+                    System.out.println("\t"+command);
+                }
+                else{
+                    toExit = true;
+                }
+            }
+            finally {
+                showLine();
+            }
+        }
         showFarewell();
     }
 
+    private static String readCommand(Scanner scan) {
+        return scan.nextLine();
+    }
+
     public static void showLine() {
-        System.out.println("_____________________________________________________________");
+        System.out.println("\t_____________________________________________________________");
     }
 
     public static void showFarewell() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\tBye. Hope to see you again soon!");
         showLine();
     }
+
 }
