eb0a0df06ca02dc17340c253642bcdd550e0f3e1
yinyin377 <54065563+yinyin924@users.noreply.github.com>
2022-02-24 19:53:27 +0800

    B-ViewSchedules

--- src/test/java/ExitCommandTest.java
+++ src/test/java/ExitCommandTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExitCommandTest {
     @Test
-    void ExitCommand_Return_ExitIsTrue_ByDefault_Test() {
+    void exitCommand_Return_ExitIsTrue_ByDefault_Test() {
         assertEquals(true,new ExitCommand().isExit());
     }
 }
